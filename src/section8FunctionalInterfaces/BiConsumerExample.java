f04be26531d08f3ad1e8b87818ed0cff2e041230
Zakaria Jouahri <zakaria.jouahri@outlook.de>
2022-05-16 17:33:28 +0200

    BiConsumer Functional Interfaces

---
+++ src/section8FunctionalInterfaces/BiConsumerExample.java
@@ -0,0 +1,43 @@
+package section8FunctionalInterfaces;
+
+import data.Student;
+import data.StudentDataBase;
+
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+public class BiConsumerExample {
+
+    public static void printNameAndActivities() {
+        BiConsumer<String, List<String>> biConsumer = (name, activities) -> {
+            System.out.println(name + " : " + activities);
+        };
+        List<Student> studentList = StudentDataBase.getAllStudents();
+
+        studentList.forEach(student -> {
+            biConsumer.accept(student.getName(), student.getActivities());
+        });
+    }
+
+    public static void main(String[] args) {
+
+        BiConsumer<String, String> biConsumer = (a, b) -> {
+            System.out.println("value of a is: " + a);
+            System.out.println("value of b is: " + b);
+        };
+
+        BiConsumer<Integer, Integer> multiply = (x, y) -> {
+            System.out.println("multiply: x * y = " + x * y);
+        };
+
+        BiConsumer<Integer, Integer> division = (x, y) -> {
+            System.out.println("division: x * y = " + x / y);
+        };
+
+        biConsumer.accept("Java8", "JavaScript");
+        multiply.andThen(division).accept(7, 3);
+
+        printNameAndActivities();
+    }
+}
